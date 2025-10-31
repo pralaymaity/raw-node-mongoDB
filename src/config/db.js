941b2d1148d859c34e1a653c6c051fe35ca4d599
prlaymaity <pralaymaity283@gmail.com>
2025-10-31 16:37:52 +0530

    update db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,25 @@
 import { MongoClient } from "mongodb";
 
-const url = "mongodb+srv://pralaymaity283_db_user:0QVz0dumBBKNsWce@testdb.vttjb2f.mongodb.net/";
-  
+const url =
+  "mongodb+srv://pralaymaity283_db_user:0QVz0dumBBKNsWce@testdb.vttjb2f.mongodb.net/";
+
 const dbName = "practiceDB";
+
+let client;
+let db;
 
 // Connect to MongoDB (only once)
 export async function connectDB() {
-
-  const client = new MongoClient(url);
-  await client.connect();
-  console.log("✅ Connected to MongoDB Atlas");
-  return client.db(dbName);
-
+  if (!client) {
+    client = new MongoClient(url);
+    await client.connect();
+    console.log("✅ Connected to MongoDB Atlas");
+    db = client.db(dbName) 
+  }
+  return db
 }
 
 export const getUserCollection = async () => {
-
   const db = await connectDB();
   return db.collection("users");
-
 };
